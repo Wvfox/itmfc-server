802d6774f9643b6a82a59e961c6c1bc1fe8d5289
unknown <Violettafox767@gmail.com>
2025-04-09 13:05:47 +0300

    fix 4

--- personal/models.py
+++ personal/models.py
@@ -76,7 +76,7 @@ class Guest(models.Model):
     created_at = models.DateField('Date of creation', auto_now_add=True)
 
     class Meta:
-        ordering = ['last_name']
+        ordering = ['created_at']
 
     def __str__(self):
         return self.user_id
