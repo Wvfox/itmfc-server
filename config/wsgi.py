d9be058cbeecb68db6838492a694213be0bd5de6
unknown <Violettafox767@gmail.com>
2025-04-30 12:06:06 +0300

    some fix 2

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,8 +10,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
-sys.path.append('/var/www/apitest.itmfc.ru')
-sys.path.append('/var/www/apitest.itmfc.ru/config')
+# sys.path.append('/var/www/apitest.itmfc.ru')
+# sys.path.append('/var/www/apitest.itmfc.ru/config')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
